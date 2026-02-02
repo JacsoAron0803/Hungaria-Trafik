96005a55503884cfc1d6c9b5cd6f68d2df37ec0c
csillagpeter <csillagpeter@turr.hu>
2026-02-02 12:56:30 +0100

    konstruktorok

--- Hungaria-Trafik/Hungaria-Trafik/Model/Termekek.cs
+++ Hungaria-Trafik/Hungaria-Trafik/Model/Termekek.cs
@@ -8,5 +8,37 @@ namespace Hungaria_Trafik.Model
 {
     internal class Termekek
     {
+        public Termekek(int id, string name, string category, int price, int stock, int age_limit)
+
+        {
+
+            this.id = id;
+
+            this.name = name;
+
+            this.category = category;
+
+            this.price = price;
+
+            this.stock = stock;
+
+            this.age_limit = age_limit;
+
+        }
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string category { get; set; }
+
+        public int price { get; set; }
+
+        public int stock { get; set; }
+
+        public int age_limit { get; set; }
+
     }
+
 }
+
